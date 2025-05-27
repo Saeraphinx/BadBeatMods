bc079c4c9e9123c196140bd7ba8869d4715d3250
Saera <saeraphinx@gmail.com>
2025-05-27 15:35:43 -0500

    fix Legacy BeatMods endpoints do not respond to status parameter #87

--- src/api/routes/beatmods.ts
+++ src/api/routes/beatmods.ts
@@ -134,12 +134,12 @@ export class BeatModsRoutes {
         }
 
         let showUnverified = status !== `approved`;
-        let statuses = showUnverified ? [Status.Verified, Status.Unverified] : [Status.Verified];
+        let statuses = showUnverified ? [Status.Verified, Status.Unverified, Status.Pending] : [Status.Verified];
 
         if (gameVersion) {
             let mods = await gameVersion.getSupportedMods(Platform.UniversalPC, statuses);
             for (let mod of mods) {
-                let convertedMod = await this.convertToBeatmodsMod(mod.mod, mod.latest, gameVersion, true);
+                let convertedMod = await this.convertToBeatmodsMod(mod.mod, mod.latest, gameVersion, true, statuses);
                 if (!convertedMod) {
                     Logger.debugWarn(`Failed to convert mod ${mod.mod.name} v${mod.latest.modVersion.raw} to BeatMods format.`, `getMod`);
                     continue;
@@ -188,7 +188,7 @@ export class BeatModsRoutes {
                     continue;
                 }
 
-                let bmMod = await this.convertToBeatmodsMod(mod, modVersion, null, true);
+                let bmMod = await this.convertToBeatmodsMod(mod, modVersion, null, true, statuses);
                 if (!bmMod) {
                     Logger.debugWarn(`Failed to convert mod ${mod.name} v${modVersion.modVersion.raw} to BeatMods format.`, `getMod`);
                     continue;
@@ -199,11 +199,11 @@ export class BeatModsRoutes {
         return res.status(200).send(modArray);
     }
 
-    private async convertToBeatmodsMod(mod: Mod, modVersion: ModVersion, gameVersion: GameVersion|null, doResolution: boolean = true): Promise<BeatModsMod|null> {
+    private async convertToBeatmodsMod(mod: Mod, modVersion: ModVersion, gameVersion: GameVersion|null, doResolution: boolean = true, statuses = [Status.Verified]): Promise<BeatModsMod|null> {
         let dependencies: (BeatModsMod | string)[] = [];
         let mVDeps: ModVersion[] = [];
         if (gameVersion) {
-            let intmVDeps = await modVersion.getLiveDependencies(gameVersion.id, [Status.Verified]);
+            let intmVDeps = await modVersion.getLiveDependencies(gameVersion.id, statuses);
             if (!intmVDeps) {
                 return null;
             }
